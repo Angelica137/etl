16eea41906443928ada6932a74846830c0038968
Angelica <Angelica137@users.noreply.github.com>
2022-08-04 20:29:28 +0100

    remove print statement

--- scripts/etl.py
+++ scripts/etl.py
@@ -1,7 +1,6 @@
 def etl(values: dict) -> dict:
     scores = {}
     for k in values.keys():
-        print(values[k])
         for v in values[k]:
             v = v.lower()
             scores[v] = k
