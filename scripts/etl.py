d28fc1da3cab035ae4bc4e875553490636a0c656
Angelica <Angelica137@users.noreply.github.com>
2022-08-04 20:29:06 +0100

    amend new systm to return lowercase

--- scripts/etl.py
+++ scripts/etl.py
@@ -3,5 +3,6 @@ def etl(values: dict) -> dict:
     for k in values.keys():
         print(values[k])
         for v in values[k]:
+            v = v.lower()
             scores[v] = k
     return scores
--- tests/test_elt.py
+++ tests/test_elt.py
@@ -14,14 +14,14 @@ values = {
 
 def test_return_value_of_a():
     v = {1: ["A"]}
-    assert etl(v) == {"A": 1}
+    assert etl(v) == {"a": 1}
 
 
 def test_returns_new_system_for_U_and_T():
     v = {1: ["A", "U",  "T"]}
-    assert etl(v) == {"A": 1, "U": 1, "T": 1}
+    assert etl(v) == {"a": 1, "u": 1, "t": 1}
 
 
 def test_returns_new_system_always():
     v = {1: ["A", "U",  "T"], 2: ["D", "G"], 5: ["K"]}
-    assert etl(v) == {"A": 1, "U": 1, "T": 1, "D": 2, "G": 2, "K": 5}
+    assert etl(v) == {"a": 1, "u": 1, "t": 1, "d": 2, "g": 2, "k": 5}
