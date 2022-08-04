5cceda13d3d49f9845c3d64ed0951f52edc9c787
Angelica <Angelica137@users.noreply.github.com>
2022-08-04 19:59:08 +0100

    etl returns value of a, red

---
+++ tests/test_elt.py
@@ -0,0 +1,16 @@
+from scripts.etl import etl
+
+
+values = {
+    1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+    2: ["D", "G"],
+    3: ["B", "C", "M", "P"],
+    4: ["F", "H", "V", "W", "Y"],
+    5: ["K"],
+    8: ["J", "X"],
+    10: ["Q", "Z"],
+}
+
+
+def test_return_vlue_of_a():
+    assert etl(values) == 1
